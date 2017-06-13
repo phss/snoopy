2a401f6b7dd6290532dc7b3f9e5cd0c0ba8fb773
Paulo Schneider <paulo.schneider@gmail.com>
2017-06-13 21:59:03 +0100

    Include headers in proxied response

--- proxy.go
+++ proxy.go
@@ -10,6 +10,12 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		resp, _ := http.Get("http://example.com/")
+		for name, values := range resp.Header {
+			for _, value := range values {
+				w.Header().Add(name, value)
+			}
+		}
+
 		defer resp.Body.Close()
 		body, _ := ioutil.ReadAll(resp.Body)
 		fmt.Fprintf(w, "%s", body)
