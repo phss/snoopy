f4444e359b1a8e850019b92b44cccdb5b728ac33
Paulo Schneider <paulo.schneider@gmail.com>
2017-06-14 22:19:09 +0100

    Parametrise proxied URL

--- proxy.go
+++ proxy.go
@@ -5,11 +5,14 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	proxiedUrl := os.Args[1]
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		resp, _ := http.Get("http://example.com/")
+		resp, _ := http.Get(proxiedUrl)
 		for name, values := range resp.Header {
 			for _, value := range values {
 				w.Header().Add(name, value)
