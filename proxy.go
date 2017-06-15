0d9119ffc541cf80d4a028c958e1d6d0ffc4e19d
Paulo Schneider <paulo.schneider@gmail.com>
2017-06-15 22:17:05 +0100

    Extract printing functions

--- proxy.go
+++ proxy.go
@@ -9,6 +9,29 @@ import (
 	"os"
 )
 
+func printRequest(proxiedUrl string, r *http.Request) {
+	fmt.Println("---")
+	color.Blue("Request")
+	color.Green("%s %s -> %s", r.Method, r.URL.Path, proxiedUrl)
+	for name, values := range r.Header {
+		for _, value := range values {
+			fmt.Printf("%s: %s\n", color.CyanString(name), color.YellowString(value))
+		}
+	}
+	fmt.Println("")
+}
+
+func printResponse(resp *http.Response) {
+	color.Blue("Response")
+	for name, values := range resp.Header {
+		for _, value := range values {
+			fmt.Printf("%s: %s\n", color.CyanString(name), color.YellowString(value))
+		}
+	}
+	//fmt.Println("")
+	//fmt.Println(string(body))
+}
+
 func main() {
 	port := 8080
 	proxiedBaseUrl := os.Args[1]
@@ -16,15 +39,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		proxiedUrl := proxiedBaseUrl + r.URL.Path
 
-		fmt.Println("---")
-		color.Blue("Request")
-		color.Green("%s %s -> %s", r.Method, r.URL.Path, proxiedUrl)
-		for name, values := range r.Header {
-			for _, value := range values {
-				fmt.Printf("%s: %s\n", color.CyanString(name), color.YellowString(value))
-			}
-		}
-		fmt.Println("")
+		printRequest(proxiedUrl, r)
 
 		resp, _ := http.Get(proxiedUrl)
 		for name, values := range resp.Header {
@@ -37,14 +52,7 @@ func main() {
 		body, _ := ioutil.ReadAll(resp.Body)
 		fmt.Fprintf(w, "%s", body)
 
-		color.Blue("Response")
-		for name, values := range resp.Header {
-			for _, value := range values {
-				fmt.Printf("%s: %s\n", color.CyanString(name), color.YellowString(value))
-			}
-		}
-		//fmt.Println("")
-		//fmt.Println(string(body))
+		printResponse(resp)
 	})
 
 	fmt.Printf("Proxying for %s on http://localhost:%d \n", proxiedBaseUrl, port)
