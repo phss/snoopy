82d103e7d0ed28e0fcc12035932bd9da95fe4c5f
Paulo Schneider <paulo.schneider@gmail.com>
2017-06-15 21:55:30 +0100

    Colorful output

--- proxy.go
+++ proxy.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/fatih/color"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -15,7 +16,15 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		proxiedUrl := proxiedBaseUrl + r.URL.Path
 
-		fmt.Printf("%s request to %s -> %s \n", r.Method, r.URL.Path, proxiedUrl)
+		fmt.Println("---")
+		color.Blue("Request")
+		color.Green("%s %s -> %s", r.Method, r.URL.Path, proxiedUrl)
+		for name, values := range r.Header {
+			for _, value := range values {
+				fmt.Printf("%s: %s\n", color.CyanString(name), color.YellowString(value))
+			}
+		}
+		fmt.Println("")
 
 		resp, _ := http.Get(proxiedUrl)
 		for name, values := range resp.Header {
@@ -27,6 +36,15 @@ func main() {
 		defer resp.Body.Close()
 		body, _ := ioutil.ReadAll(resp.Body)
 		fmt.Fprintf(w, "%s", body)
+
+		color.Blue("Response")
+		for name, values := range resp.Header {
+			for _, value := range values {
+				fmt.Printf("%s: %s\n", color.CyanString(name), color.YellowString(value))
+			}
+		}
+		//fmt.Println("")
+		//fmt.Println(string(body))
 	})
 
 	fmt.Printf("Proxying for %s on http://localhost:%d \n", proxiedBaseUrl, port)
