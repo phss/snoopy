650b25b8276e63720d7b89f7b285b9ae7425b08b
Paulo Schneider <paulo.schneider@gmail.com>
2017-06-16 22:05:11 +0100

    Extract methods to make proxy request and return response

--- proxy.go
+++ proxy.go
@@ -32,27 +32,33 @@ func printResponse(resp *http.Response) {
 	//fmt.Println(string(body))
 }
 
+func makeProxyRequest(proxiedUrl string) *http.Response {
+	resp, _ := http.Get(proxiedUrl)
+	return resp
+}
+
+func returnProxyResponse(resp *http.Response, w http.ResponseWriter) {
+	for name, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(name, value)
+		}
+	}
+
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Fprintf(w, "%s", body)
+}
+
 func main() {
 	port := 8080
 	proxiedBaseUrl := os.Args[1]
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		proxiedUrl := proxiedBaseUrl + r.URL.Path
-
 		printRequest(proxiedUrl, r)
-
-		resp, _ := http.Get(proxiedUrl)
-		for name, values := range resp.Header {
-			for _, value := range values {
-				w.Header().Add(name, value)
-			}
-		}
-
-		defer resp.Body.Close()
-		body, _ := ioutil.ReadAll(resp.Body)
-		fmt.Fprintf(w, "%s", body)
-
+		resp := makeProxyRequest(proxiedUrl)
 		printResponse(resp)
+		returnProxyResponse(resp, w)
 	})
 
 	fmt.Printf("Proxying for %s on http://localhost:%d \n", proxiedBaseUrl, port)
