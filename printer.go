db21d14371147a5b3ad74daab32d72a3f43a13d9
Paulo Schneider <paulo.schneider@gmail.com>
2017-06-24 16:34:56 +0100

    Extract functions to print headers and body

--- printer.go
+++ printer.go
@@ -5,28 +5,31 @@ import (
 	"github.com/fatih/color"
 )
 
-func printRequest(proxyRequest ProxyRequest, config Config) {
+func printRequest(request ProxyRequest, config Config) {
 	fmt.Println("---")
 	color.Blue("Request")
-	color.Green("%s %s -> %s", proxyRequest.Method, proxyRequest.Path, proxyRequest.ProxiedUrl())
-	for _, header := range proxyRequest.Headers {
-		fmt.Printf("%s: %s\n", color.CyanString(header.Name), color.YellowString(header.Value))
-	}
-	if config.ShowBody {
-		fmt.Printf("%s:\n", color.CyanString("Body"))
-		fmt.Println(string(proxyRequest.Body))
-	}
+	color.Green("%s %s -> %s", request.Method, request.Path, request.ProxiedUrl())
+	printHeaders(request.Headers)
+	printBody(request.Body, config)
 	fmt.Println("")
 }
 
 func printResponse(response ProxyResponse, config Config) {
 	color.Blue("Response")
 	fmt.Printf("%s: %s\n", color.CyanString("Status"), color.YellowString(response.Status))
-	for _, header := range response.Headers {
+	printHeaders(response.Headers)
+	printBody(response.Body, config)
+}
+
+func printHeaders(headers []ProxyHeader) {
+	for _, header := range headers {
 		fmt.Printf("%s: %s\n", color.CyanString(header.Name), color.YellowString(header.Value))
 	}
+}
+
+func printBody(body []byte, config Config) {
 	if config.ShowBody {
 		fmt.Printf("%s:\n", color.CyanString("Body"))
-		fmt.Println(string(response.Body))
+		fmt.Println(string(body))
 	}
 }
